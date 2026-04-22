14e0f9d46239c1698a6eb98e41aaf91469a3c813
knelson40-matc <knelson40@madisoncollege.edu>
2026-04-22 17:55:46 -0500

    Modified App.js to have my name and project name

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Kaley Nelson, Project 4!!!</h1>
+      <h1>Kaley Nelson, Project 4, CI/CD!!!</h1>
     </div>
   );
 }
